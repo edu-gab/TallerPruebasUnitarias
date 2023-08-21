3bc326fb7fff506f9abc4295b4d1564bb848ab36
jair-chaguay <jchaguay@espol.edu.ec>
2023-08-21 13:20:10 -0500

    Testing metodo cs

--- TallerPruebas/src/Test.java
+++ TallerPruebas/src/Test.java
@@ -26,6 +26,28 @@ public class Test extends TestCase {
 	public void testEuro() {
 		assertEquals(573f, supervisorEuro.CalculateYearBonus());
 	}
+	
+	//Test que comprueba el calculo del salario dependiendo del trabajador si usa otra moneda
+	public void testEurocs() {
+		assertEquals(381.75f,supervisorEuro.cs());
+		
+	}
+	// Test que comprueba el calculo del salario del Empleado tipo Worker
+	public void testcsWorker() {
+		assertEquals(400f,empleado.cs());
+	}
+	
+	// Test que comprueba el calculo del salario del Empleado tipo Worker
+	public void testcsSupervisor() {
+		assertEquals(401.75f,supervisor.cs());
+	}
+	
+	public void testcsManager() {
+		assertEquals(403.5f,gerente.cs());
+	}
+	
+	
+	
 }
 
 
